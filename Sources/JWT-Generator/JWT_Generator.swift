e241b036b00d61cef1069512dbfd9c5e25a55283
Nick Kibysh <nick.kibysh@gmail.com>
2023-02-27 12:59:32 +0100

    Initial commit

---
+++ Sources/JWT-Generator/JWT_Generator.swift
@@ -0,0 +1,8 @@
+@main
+public struct JWT_Generator {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(JWT_Generator().text)
+    }
+}
---
+++ Tests/JWT-GeneratorTests/JWT_GeneratorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import JWT_Generator
+
+final class JWT_GeneratorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(JWT_Generator().text, "Hello, World!")
+    }
+}
